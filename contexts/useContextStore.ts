e4a741021995a999b092577e4dbcf039f999c486
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2023-01-29 22:24:17 +0530

    Fix context store localstorage issue

--- contexts/useContextStore.ts
+++ contexts/useContextStore.ts
@@ -1,17 +1,48 @@
 "use client"
 
 import { useEffect, useState } from "react";
+const STORE_KEY_INITIAL = "context_"
+
+const store = (key: string, value: any = null) => {
+  if(!value) {
+    const item = localStorage.getItem(key)
+    if(!item) return
+    return item
+  }
+  localStorage.setItem(key, value)
+}
+
 
 export default function usePersistentContextStore(key: string, value: any) {
-  const storeKey = "context_"+ key
-  const [state, setState] = useState(() => {
-    const persistedState = localStorage.getItem(storeKey);
-    return persistedState ? JSON.parse(persistedState) : value;
-  });
+  let initialValue = value
+  let storeKey = STORE_KEY_INITIAL + key
+  const isServer = typeof window === "undefined"
+
+  const [state, setState] = useState(initialValue)
+  const [initStateRender, setInitStateRender] = useState(true)
 
   useEffect(() => {
-    localStorage.setItem(storeKey, JSON.stringify(state));
-  }, [storeKey, state]);
+    if(!isServer) {
+      const storedItem = store(storeKey)
+      if(initStateRender) {
+        setInitStateRender(false)
+        if(storedItem) {
+          setState(JSON.parse(storedItem))
+        } else {
+          store(storeKey, JSON.stringify(initialValue))
+        }
+      }
+    }
+  }, [])
+
+
+  useEffect(() => {
+    if(!isServer && state) {
+      if(initStateRender) return
+      console.log("setting user", key, state)
+      store(storeKey, JSON.stringify(state))
+    }
+  }, [state]);
 
   return [state, setState];
 }
