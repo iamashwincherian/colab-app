3972b9322dc6b0fe40598b1b2f338e23ee002e9a
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2023-07-27 06:11:48 +0530

    Change newList button design

--- src/components/kanban/forms/newList.tsx
+++ src/components/kanban/forms/newList.tsx
@@ -32,10 +32,10 @@ export default function NewListForm({ onCreate }: { onCreate: Function }) {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogTrigger onClick={() => setOpen(true)}>
-        <div className="bg-gray-50 shadow-sm w-64 mr-4 text-left flex flex-col border dark:border-none dark:bg-dark-2 rounded-md cursor-pointer">
-          <div className="p-2 dark:shadow-xl px-3">Create a new List</div>
-          <div className="flex justify-center w-full bg-gray-50 hover:bg-gray-100 dark:bg-dark-2 py-2 h dark:hover:bg-dark-3 rounded-b-md">
-            <PlusIcon className="w-5 h-5 [&>path]:stroke-[3]" />
+        <div className="bg-gray-50 shadow-sm w-64 mr-4 text-left flex flex-col border-2 border-dotted dark:border-none dark:bg-dark-2 rounded-md cursor-pointer">
+          <div className="flex items-center p-2 dark:shadow-xl px-3">
+            <PlusIcon className="w-5 h-5 [&>path]:stroke-[3] mr-2" />
+            Create a new List
           </div>
         </div>
       </DialogTrigger>
