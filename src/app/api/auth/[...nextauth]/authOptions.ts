3231f6fce4a382ff225d3fec824f203f6e478da4
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2023-08-04 23:26:15 +0530

    Fix authOptions session callback

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -40,11 +40,4 @@ export const authOptions: NextAuthOptions = {
     error: "/auth/error", // Error code passed in query string as ?error=
     verifyRequest: "/auth/verify-request", // (used for check email message)
   },
-  callbacks: {
-    session(props) {
-      const { session, user } = props;
-      session.user = user;
-      return session;
-    },
-  },
 };
--- src/utils/useUser.tsx
+++ src/utils/useUser.tsx
@@ -3,8 +3,6 @@ import { useSession } from "next-auth/react";
 export default function useUser() {
   const { data, status } = useSession();
 
-  console.log("useUser", status, data);
-
   if (status === "authenticated") {
     if (data) {
       return data.user;
