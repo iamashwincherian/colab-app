35196c35c1c9e50787207d28c5f6f52724348d9b
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2023-08-06 15:16:17 +0530

    Move kanban header back to the main route page

--- src/app/boards/[boardId]/page.tsx
+++ src/app/boards/[boardId]/page.tsx
@@ -9,6 +9,13 @@ import FullScreenLayout from "../../../components/layouts/FullScreenLayout";
 import useBoard from "./hooks/useBoard.hook";
 import withAuth from "../../../utils/withAuth";
 import { FC } from "react";
+import { Button } from "@components/ui/button";
+import BoardNameEditor from "@components/input/boardNameEditor/BoardNameEditor";
+import openModal from "@utils/openModal";
+import CreateListModal from "@components/kanban/modals/CreateListModal";
+import { useBoardContext } from "@contexts/BoardContext";
+import Loading from "@components/loading/Loading";
+import { GearIcon, PlusIcon } from "@radix-ui/react-icons";
 
 type BoardProps = {
   params: { boardId: string };
@@ -19,6 +26,7 @@ const Board = (props: BoardProps) => {
     params: { boardId },
   } = props;
   const { board, list, cards } = useBoard(boardId);
+  const { createList } = useBoardContext();
 
   const breadCrumbs = (
     <BreadCrumbs>
@@ -27,12 +35,49 @@ const Board = (props: BoardProps) => {
     </BreadCrumbs>
   );
 
+  const createNewListButton = (boardId: number) => {
+    if (!boardId) return;
+    return (
+      <Button
+        onClick={() =>
+          openModal(
+            <CreateListModal
+              onSubmit={({ name }: { name: string }) =>
+                createList({ boardId, name })
+              }
+            />
+          )
+        }
+      >
+        <PlusIcon className="mr-2" />
+        Create New
+      </Button>
+    );
+  };
+
+  const openSettings = () => {};
+
+  if (!board) return <Loading />;
+
   return (
     <FullScreenLayout nav>
       <div className="p-10 pt-6">
-        {board && breadCrumbs}
+        <div className="flex justify-between items-center">
+          <BoardNameEditor name={board.name} />
+          <div className="flex justify-center gap-2">
+            {createNewListButton(board.id)}
+            <Button
+              onClick={openSettings}
+              variant="outline"
+              className="w-9 h-9 px-2"
+            >
+              <GearIcon />
+            </Button>
+          </div>
+        </div>
+        {breadCrumbs}
         <div className="mt-4">
-          {board && <KanbanBoard board={board} list={list} cards={cards} />}
+          <KanbanBoard board={board} list={list} cards={cards} />
         </div>
       </div>
     </FullScreenLayout>
--- src/components/kanban/KanbanBoard.tsx
+++ src/components/kanban/KanbanBoard.tsx
@@ -2,16 +2,10 @@
 
 import { useEffect } from "react";
 import { DragDropContext, DropResult } from "react-beautiful-dnd";
-import { PlusIcon } from "@radix-ui/react-icons";
 
 import List from "./List";
 import useKanbanBoard from "./hooks/useKanbanBoard";
 import { BoardProp, ListProp, CardProp } from "./types";
-import { trpc } from "../../utils/trpc/trpc";
-import openModal from "../../utils/openModal";
-import BoardNameEditor from "../input/boardNameEditor/BoardNameEditor";
-import { Button } from "../ui/button";
-import CreateListModal from "./modals/CreateListModal";
 import { useBoardContext } from "../../contexts/BoardContext";
 import { sortList } from "./helpers/sort";
 
@@ -25,7 +19,7 @@ export default function KanbanBoard(props: KanbanBoardProps) {
   const { createList, editList, createCard } = useBoardContext();
 
   const { list: defaultList, cards: defaultCards } = props;
-  let { list, cards, onDragEnd, updateList, updateCards } = useKanbanBoard(
+  let { list, cards, onDragEnd, updateList } = useKanbanBoard(
     defaultList,
     defaultCards
   );
@@ -37,7 +31,6 @@ export default function KanbanBoard(props: KanbanBoardProps) {
 
   if (!props.board) return <></>;
   const {
-    board,
     board: { id: boardId },
   } = props;
 
@@ -45,54 +38,31 @@ export default function KanbanBoard(props: KanbanBoardProps) {
     await onDragEnd(boardId, result);
   };
 
-  const createNewButton = (
-    <Button
-      onClick={() =>
-        openModal(
-          <CreateListModal
-            onSubmit={({ name }: { name: string }) =>
-              createList({ boardId, name })
-            }
-          />
-        )
-      }
-    >
-      <PlusIcon className="mr-2" />
-      Create New
-    </Button>
-  );
-
   return (
-    <div>
-      <div className="flex justify-between items-center">
-        <BoardNameEditor name={board.name} />
-        {createNewButton}
-      </div>
-      <div className="mt-6">
-        <DragDropContext onDragEnd={handleOnChange}>
-          <div className="flex dark:text-white items-start">
-            {sortedList &&
-              (sortedList?.length
-                ? sortedList.map(({ id, name }: any) => {
-                    const cardsInTheListItem = cards.filter(
-                      (card) => card?.listId === id
-                    );
-                    return (
-                      <List
-                        id={id}
-                        key={id}
-                        name={name}
-                        boardId={boardId}
-                        cards={cardsInTheListItem}
-                        onEdit={editList}
-                        onCardCreate={createCard}
-                      />
-                    );
-                  })
-                : null)}
-          </div>
-        </DragDropContext>
-      </div>
+    <div className="mt-8">
+      <DragDropContext onDragEnd={handleOnChange}>
+        <div className="flex dark:text-white items-start">
+          {sortedList &&
+            (sortedList?.length
+              ? sortedList.map(({ id, name }: any) => {
+                  const cardsInTheListItem = cards.filter(
+                    (card) => card?.listId === id
+                  );
+                  return (
+                    <List
+                      id={id}
+                      key={id}
+                      name={name}
+                      boardId={boardId}
+                      cards={cardsInTheListItem}
+                      onEdit={editList}
+                      onCardCreate={createCard}
+                    />
+                  );
+                })
+              : null)}
+        </div>
+      </DragDropContext>
     </div>
   );
 }
