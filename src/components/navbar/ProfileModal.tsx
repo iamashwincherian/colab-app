789dab8fa72100fa33d4b6c10a4ece67262482fa
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2023-10-11 22:32:13 +0530

    Profile modal wip

--- src/components/navbar/ProfileModal.tsx
+++ src/components/navbar/ProfileModal.tsx
@@ -1,5 +1,9 @@
 import PrimaryModal from "../modals/PrimaryModal";
 
 export default function ProfileModal() {
-  return <PrimaryModal title="Profile"></PrimaryModal>;
+  return (
+    <PrimaryModal title="Profile">
+      <div>Profile Modal Content</div>
+    </PrimaryModal>
+  );
 }
