19d482941f11265802d9b8186a987aaf45552d95
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2024-01-16 21:28:58 +0530

    Update verificationToken table

---
+++ prisma/migrations/20240116140402_update_verification_token/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `identifier` on the `VerificationToken` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[userId,token]` on the table `VerificationToken` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `userId` to the `VerificationToken` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "VerificationToken_identifier_token_key";
+
+-- AlterTable
+ALTER TABLE "VerificationToken" DROP COLUMN "identifier",
+ADD COLUMN     "userId" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_userId_token_key" ON "VerificationToken"("userId", "token");
+
+-- AddForeignKey
+ALTER TABLE "VerificationToken" ADD CONSTRAINT "VerificationToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
